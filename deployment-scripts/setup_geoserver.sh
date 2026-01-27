db8324e03ced3bde25637f31705500a63806dd14
Fred Otieno <fred.otieno@ibm.com>
2026-01-27 21:52:41 +0300

    chore: :wrench: reforward port after failed curl request

--- deployment-scripts/setup_geoserver.sh
+++ deployment-scripts/setup_geoserver.sh
@@ -3,11 +3,17 @@
 
 
 # Check if geoserver is ready
-sleep 10
-echo "Loading \\c"
+sleep 5
+printf "Loading \n"
 
-until [ "$(curl -s -u $GEOSERVER_USERNAME:$GEOSERVER_PASSWORD -o /dev/null -w "%{http_code}" http://localhost:3000/geoserver/rest/workspaces)" == "200" ]; do sleep 5; done
-echo "\nUpdating settings"
+until curl --silent --show-error -u $GEOSERVER_USERNAME:$GEOSERVER_PASSWORD -f http://localhost:3000/geoserver/rest/workspaces > /dev/null; do
+  echo "Endpoint not available yet. Sleeping for 5 seconds..."
+  kubectl port-forward -n $OC_PROJECT svc/geofm-geoserver 3000:3000 >> studio-pf.log 2>&1 &
+  kill -9 $(lsof -t -i:3000) > /dev/null 2>&1
+  sleep 5
+done
+
+printf "\nUpdating settings\n"
 
 # Create workspace
 curl --silent --show-error -u $GEOSERVER_USERNAME:$GEOSERVER_PASSWORD -L -X POST http://localhost:3000/geoserver/rest/workspaces \
@@ -25,3 +31,5 @@ curl --silent --show-error -u $GEOSERVER_USERNAME:$GEOSERVER_PASSWORD -L -X PUT 
 curl --silent --show-error -u $GEOSERVER_USERNAME:$GEOSERVER_PASSWORD -L -X POST http://localhost:3000/geoserver/rest/security/acl/services \
 --header "Content-type: application/json" \
 --data '{"gwc.*":"*","wcs.*":"*","wfs.*":"*","wms.*":"*"}'
+
+printf "\nCompleted geoserver configuration\n"
