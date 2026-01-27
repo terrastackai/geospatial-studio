533a4f24c3db5a58cbc19d8df7e1ada10e20953a
Fred Otieno <fred.otieno@ibm.com>
2026-01-27 15:36:44 +0300

    chore: :memo:

--- deploy_studio_cluster.sh
+++ deploy_studio_cluster.sh
@@ -202,6 +202,7 @@ if [[ "$JUMP_TO_DEPLOYMENT" == "No" ]]; then
     echo "**********************************************************************"
     echo "-----------  Configure s3 storage classes --------------"
     echo "--- You should already have setup the cloud object storage drivers ---"
+    echo "-- See: https://cloud.ibm.com/docs/openshift?topic=openshift-storage_cos_install --"
     echo "**********************************************************************"
     echo "***********  Update workspace/${DEPLOYMENT_ENV}/env/env.sh ***********"
     echo "-----------  export COS_STORAGE_CLASS= -------------------------------"
