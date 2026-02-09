0286bddfb3b62f1fdbde2d82ee33b40fcb40b1ad
Brian Glar <bogolla4@gmail.com>
2026-02-09 17:28:30 +0300

    Increase helm timeout to solve context deadline exceeded error
    Signed-off-by: Brian Glar <bogolla4@gmail.com>

--- deployment-scripts/deploy_studio.sh
+++ deployment-scripts/deploy_studio.sh
@@ -11,6 +11,7 @@ helm upgrade -f workspace/$DEPLOYMENT_ENV/values/geospatial-studio/values-deploy
             ./geospatial-studio/ \
             --install \
             --wait \
+            --timeout 30m \
             --history-max 5 \
             --set "global.imagePullSecret.b64secret=${image_pull_secret_b64}" \
             --set "global.postgres.backend_uri_base=postgresql+pg8000://${pg_username}:${pg_password}@${pg_uri}:${pg_port}" \
