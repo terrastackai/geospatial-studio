b116558f60b1d03cc8fb1c6d3c7ae5fc43ed8f3a
Brian Ogolla <bogolla4@gmail.com>
2026-04-01 20:18:58 +0300

    Update deployment-scripts/deploy_studio.sh

--- deployment-scripts/deploy_studio.sh
+++ deployment-scripts/deploy_studio.sh
@@ -12,7 +12,6 @@ helm upgrade -f workspace/$DEPLOYMENT_ENV/values/geospatial-studio/values-deploy
             --install \
             --namespace ${OC_PROJECT} \
             --wait \
-            --wait-for-resources=deployments,statefulsets,daemonsets,replicasets,pods,jobs \
             --timeout 30m \
             --history-max 5 \
             --set "global.imagePullPolicy=${image_pull_policy}" \
